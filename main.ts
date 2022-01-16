9fa387340b56fda047899a800bfaa5ba39fa9ffa
cinaird <kontakta.jonathan@gmail.com>
2022-01-16 07:25:12 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P0, 102)
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P0, 91)
+    basic.showIcon(IconNames.Tortoise)
+})
+tinkercademy.crashSensorSetup(DigitalPin.P5)
 basic.forever(function () {
-	
+    if (tinkercademy.crashSensor()) {
+        pins.servoWritePin(AnalogPin.P0, 139)
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
