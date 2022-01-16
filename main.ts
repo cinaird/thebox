3551474f253312434ed31ad02ae85194c9a03de9
cinaird <kontakta.jonathan@gmail.com>
2022-01-16 14:16:44 +0100

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,38 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P0, 102)
+    Kika()
     basic.showIcon(IconNames.Yes)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P0, 91)
+    pins.servoWritePin(AnalogPin.P0, 170)
     basic.showIcon(IconNames.Tortoise)
 })
+function Kika () {
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P0, 110)
+    basic.pause(1500)
+    pins.servoWritePin(AnalogPin.P0, 80)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P0, 170)
+}
+let TTP = 0
+let snabbhet = 0
 tinkercademy.crashSensorSetup(DigitalPin.P5)
 basic.forever(function () {
     if (tinkercademy.crashSensor()) {
-        pins.servoWritePin(AnalogPin.P0, 139)
+        snabbhet = randint(1, 9)
+        if (snabbhet < 8) {
+            TTP = snabbhet * 0
+        } else if (snabbhet == 8) {
+            Kika()
+        } else {
+            TTP = 4000
+        }
+        basic.showNumber(snabbhet)
+        basic.pause(TTP)
+        pins.servoWritePin(AnalogPin.P0, 80)
         basic.showIcon(IconNames.Happy)
+        pins.servoWritePin(AnalogPin.P0, 170)
     } else {
-        basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.SmallDiamond)
     }
 })
