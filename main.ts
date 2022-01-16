4712cab22202bf20441c233db486bdb4a374c834
cinaird <kontakta.jonathan@gmail.com>
2022-01-16 16:46:23 +0100

    lagt till reset

--- main.ts
+++ main.ts
@@ -5,13 +5,14 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 170)
     basic.showIcon(IconNames.Tortoise)
+    control.reset()
 })
 function Kika () {
     basic.pause(1000)
     pins.servoWritePin(AnalogPin.P0, 110)
-    basic.pause(1500)
+    basic.pause(2000)
     pins.servoWritePin(AnalogPin.P0, 80)
-    basic.pause(1000)
+    basic.pause(300)
     pins.servoWritePin(AnalogPin.P0, 170)
 }
 let TTP = 0
@@ -22,16 +23,20 @@ basic.forever(function () {
         snabbhet = randint(1, 9)
         if (snabbhet < 8) {
             TTP = snabbhet * 0
-        } else if (snabbhet == 8) {
-            Kika()
         } else {
             TTP = 4000
         }
         basic.showNumber(snabbhet)
-        basic.pause(TTP)
-        pins.servoWritePin(AnalogPin.P0, 80)
-        basic.showIcon(IconNames.Happy)
-        pins.servoWritePin(AnalogPin.P0, 170)
+        if (snabbhet == 8) {
+            Kika()
+        } else {
+            basic.pause(TTP)
+            pins.servoWritePin(AnalogPin.P0, 80)
+            basic.showIcon(IconNames.Happy)
+            pins.servoWritePin(AnalogPin.P0, 170)
+        }
+        basic.pause(300)
+        control.reset()
     } else {
         basic.showIcon(IconNames.SmallDiamond)
     }
